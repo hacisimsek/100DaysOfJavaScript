4e6a08f4d988ddb6e2d12c654e425a857a4b502b
Hacı Şimşek <73344347+hacisimsek@users.noreply.github.com>
2022-02-06 12:44:12 +0300

    22,23,24,25,26,27,28,29,30,31,32 of 100daysOfJs

---
+++ GeneratePasswordApp/app.js
@@ -0,0 +1,17 @@
+function generatePassword() {
+  var length = 8,
+    charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
+    retVal = '';
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
+
+const setRandomNumber = () => {
+  const randomeNumber = generatePassword();
+  return (result.innerHTML = randomeNumber);
+};
+
+generate.addEventListener('click', setRandomNumber);
+setRandomNumber();
